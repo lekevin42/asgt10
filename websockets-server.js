cc9665c07fa7ef26581018fa0721427e6c7c1541
Kevin Le <lekevin42@yahoo.com>
2017-04-19 20:45:54 -0700

    Fix eslint errors and add eslint cfg

--- websockets-server.js
+++ websockets-server.js
@@ -9,17 +9,17 @@ var messages = [];
 console.log('websockets server started');
 
 ws.on('connection', function(socket) {
-    console.log('client connection established');
+  console.log('client connection established');
 
-    messages.forEach(function(msg) {
-        socket.send(msg);
-    });
+  messages.forEach(function(msg) {
+    socket.send(msg);
+  });
 
-    socket.on('message', function(data) {
-        console.log('message received: ' + data);
-        messages.push(data);
-        ws.clients.forEach(function(clientSocket) {
-            clientSocket.send(data);
-        });
+  socket.on('message', function(data) {
+    console.log('message received: ' + data);
+    messages.push(data);
+    ws.clients.forEach(function(clientSocket) {
+      clientSocket.send(data);
     });
+  });
 });
